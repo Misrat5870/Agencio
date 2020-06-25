9ec2e5091c95be0b53c684ee53af36ad9661f602
Misrat Mitu <misratmitu@gmail.com>
2020-06-25 21:14:46 +0600

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,4 +17,61 @@ $(document).ready(function(){
     $(".search-icon span i").click(function(){
         $(".search-box").fadeToggle();
     });
-})
+
+    //service
+    $(".service li").click(function(){
+        $(".service li").removeClass("service-active");
+        $(this).addClass("service-active");
+
+        $(".li-title").removeClass("title-white");
+        $(this).children(".li-title").addClass("title-white");
+
+        $(".li-title p").removeClass("active-dot");
+        $(this).children().children(".li-title p").addClass("active-dot");
+
+        $(".li-details").slideUp();
+        $(this).children(".li-details").slideToggle();
+        
+    });
+
+
+    //Recent Work Gallery
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:15,
+        responsiveClass:true,
+        autoplay:false,
+        dots:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            481:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:true,
+                loop:false
+            }
+        }
+    });
+
+    $('.play-btn').click(function () {
+        var mediaVideo = $("video").get(0);
+        if (mediaVideo.paused) {
+            $(".play-btn").css("opacity",".1");
+            mediaVideo.play();
+            
+        } else {
+            mediaVideo.pause();
+            $(".play-btn").css("opacity","1");
+       }
+     });
+
+});
+
+
+
